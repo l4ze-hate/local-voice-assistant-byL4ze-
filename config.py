4ac7d7b131e6a58786a7b64b3f01a69d9db7bb3e
l4ze-hate <274506125+l4ze-hate@users.noreply.github.com>
2026-04-08 18:43:08 +0300

    +config_Fix_key +gitignore

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-OPEN_API_KEY = "YOUR_API_KEY"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Загружает переменные из .env
+
+OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 WAKE_WORD = "JARVIS"
 LANGUAGE = "ru-RU"
